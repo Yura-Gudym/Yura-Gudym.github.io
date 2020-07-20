f1505206d347c70a200dbec3d3f421a9467d30b6
Yura-Gudym <67923002+Yura-Gudym@users.noreply.github.com>
2020-07-20 16:11:13 +0300

    map

---
+++ School/map.php
@@ -0,0 +1,1 @@
+<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Af206f8ed8ccca5da97ffefd6808e955a3e55d312a5e923feefc70a7a0ec6dfbd&amp;width=100%25&amp;height=250&amp;lang=ru_UA&amp;scroll=true"></script>
